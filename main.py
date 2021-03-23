1b8946bf5d5c11f5d8fb6e9515873f3450ecf9bc
pali08 <p.mikulaj@gmail.com>
2021-03-23 02:38:53 +0100

    Getting maps dictionary - splitting big area to smaller areas if needed (50,000+ nodes). Unifying maps

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import osmapi.OsmApi
-
+import datetime
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -13,7 +9,7 @@ def print_hi(name):
 def get_map_with_check(*coordinates):
     try:
         return osmapi.OsmApi().Map(*coordinates)
-    except osmapi.OsmApi.ApiError as oai:
+    except osmapi.ApiError as oai:
         return 0
 
 
@@ -24,25 +20,51 @@ def split_into_bins(min_coor, max_coor, number_of_bins):
     return intervals
 
 
-def get_map(*coordinates):
+def get_maps(*coordinates):
     osm_maps = [get_map_with_check(*coordinates)]
+    # print(osm_maps[0])
+    if osm_maps != [0]:
+        print('Nodes count is ' + str(len([i for i in osm_maps[0] if i['type'] == 'node'])))
+        print(len(osm_maps[0]))
     counter = 2
     while 0 in osm_maps:
-        osm_maps = []
+        # osm_maps = []
         intervals_lon = split_into_bins(coordinates[0], coordinates[2], counter)
+        # print(intervals_lon)
         intervals_lat = split_into_bins(coordinates[1], coordinates[3], counter)
-        for i in range(0, counter):
-            osm_maps.append(get_map_with_check(intervals_lon[0], intervals_lat[0], intervals_lon[1], intervals_lat[1]))
+        osm_maps = [get_map_with_check(intervals_lon[i][0],
+                                       intervals_lat[j][0],
+                                       intervals_lon[i][1],
+                                       intervals_lat[j][1]) for i in range(0, counter) for j in range(0, counter)]
+        # print(intervals_lat)
+        # for i in range(0, counter):
+        #     for j in range(0, counter):
+        #         osm_maps.append(get_map_with_check(intervals_lon[i][0],
+        #                                            intervals_lat[j][0],
+        #                                            intervals_lon[i][1],
+        #                                            intervals_lat[j][1]))
+        # print(osm_maps)
+        # print(osm_maps)
         counter += 1
+    # try:
+    #     print(osm_maps == osm_maps_2)
+    # except UnboundLocalError:
+    #     pass
     return osm_maps
 
-def unify_maps(map_dicts):
-    pass
+
+def unify_maps(all_map_dicts):
+    id_set = set()
+    unified_map = []
+    for map_dict in all_map_dicts:
+        for item in map_dict:
+            if item['data']['id'] not in id_set:
+                id_set.add(item['data']['id'])
+                unified_map.append(item)
+    return unified_map
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    get_map()
+    osm_maps = get_maps()
+    unify_maps(osm_maps)
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
