49a4b4e01d1fc43ac88f7037b97ce89965413f93
pali08 <p.mikulaj@gmail.com>
2021-04-06 03:48:56 +0200

    playing around with qt - set save filepath

--- qt_playground.py
+++ qt_playground.py
@@ -1,9 +1,13 @@
+import os
+from pathlib import Path
+
 from PyQt5.QtGui import QPixmap
 
 from map_drawer import Ui_MainWindow
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox, QGraphicsPixmapItem, QGraphicsScene
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox, QGraphicsPixmapItem, QGraphicsScene, \
+    QFileDialog
 from PyQt5.QtCore import QFile, pyqtSlot
 
 # from ui_mainwindow import Ui_MainWindow
@@ -15,9 +19,12 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        # self.ui.pushButton
         self.ui.pushButton.clicked.connect(self.on_click)
+        self.ui.pushButtonSetPath.clicked.connect(self.on_click_set_path)
         self.show()
+
+    def set_filename(self):
+        fileName = QFileDialog.getOpenFileName(self, "Open Image", str(Path.home()), "Image Files (*.png *.jpg *.bmp)")
 
     def draw_image(self, image_path):
         pix = QPixmap(image_path)
@@ -32,17 +39,16 @@ class MainWindow(QMainWindow):
         textbox_value_lon_right = float(self.ui.lineEdit_2.text())
         textbox_value_lat_up = float(self.ui.lineEdit_3.text())
         textbox_value_lat_down = float(self.ui.lineEdit_4.text())
-        print(textbox_value_lat_down)
-        print(textbox_value_lat_up)
-        print(textbox_value_lon_right)
-        print(textbox_value_lon_left)
         get_image(textbox_value_lat_down, textbox_value_lon_left, textbox_value_lat_up, textbox_value_lon_right)
         self.draw_image('rectangle_area_map.png')
         # get_image(49.4750, 15.8611, 49.5005, 15.9178)
         QMessageBox.question(self, 'File save', 'The file was saved',
                              QMessageBox.Ok,
                              QMessageBox.Ok)
-        # self.ui.lineEdit_2.setText("")
+
+    @pyqtSlot()
+    def on_click_set_path(self):
+        self.set_filename()
 
 
 if __name__ == "__main__":
