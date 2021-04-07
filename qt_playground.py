f6e0fcb5ab6ec101d92b4c2a7cc1feba5ed183ed
pali08 <p.mikulaj@gmail.com>
2021-04-07 02:47:44 +0200

    playing around with qt - set save filepath

--- qt_playground.py
+++ qt_playground.py
@@ -23,8 +23,17 @@ class MainWindow(QMainWindow):
         self.ui.pushButtonSetPath.clicked.connect(self.on_click_set_path)
         self.show()
 
-    def set_filename(self):
-        fileName = QFileDialog.getOpenFileName(self, "Open Image", str(Path.home()), "Image Files (*.png *.jpg *.bmp)")
+    def set_filename_open(self):
+        return QFileDialog.getOpenFileName(self, "Open Image", str(Path.home()), "Image Files (*.png *.jpg *.bmp)")
+
+    def set_filename_save(self):
+        if 'PYCHARM_HOSTED' in os.environ:
+            return QFileDialog.getSaveFileName(self, caption="Save Image", directory=str(Path.home()),
+                                               filter="Image Files (*.png *.jpg *.bmp)",
+                                               options=QFileDialog.DontUseNativeDialog)
+        else:
+            return QFileDialog.getSaveFileName(self, caption="Save Image", directory=str(Path.home()),
+                                               filter="Image Files (*.png *.jpg *.bmp)")
 
     def draw_image(self, image_path):
         pix = QPixmap(image_path)
@@ -39,16 +48,28 @@ class MainWindow(QMainWindow):
         textbox_value_lon_right = float(self.ui.lineEdit_2.text())
         textbox_value_lat_up = float(self.ui.lineEdit_3.text())
         textbox_value_lat_down = float(self.ui.lineEdit_4.text())
-        get_image(textbox_value_lat_down, textbox_value_lon_left, textbox_value_lat_up, textbox_value_lon_right)
-        self.draw_image('rectangle_area_map.png')
+        image_filepath_save = self.ui.lineEdit_save_image.text()
+        while not os.path.exists(os.path.dirname(image_filepath_save)) or os.path.exists(
+                image_filepath_save):
+            QMessageBox.question(self, 'Problem saving file',
+                                 'Path not exists or file already exists. Pick another path', QMessageBox.Ok,
+                                 QMessageBox.Ok)
+            image_filepath_save = self.set_filename_save()[0]
+            print(image_filepath_save)
+            self.ui.lineEdit_save_image.setText(image_filepath_save)
+
+        get_image(textbox_value_lat_down, textbox_value_lon_left, textbox_value_lat_up, textbox_value_lon_right,
+                  filepath=image_filepath_save)
+        self.draw_image(image_filepath_save)
         # get_image(49.4750, 15.8611, 49.5005, 15.9178)
-        QMessageBox.question(self, 'File save', 'The file was saved',
+        QMessageBox.question(self, 'File save', 'The file was saved to' + image_filepath_save,
                              QMessageBox.Ok,
                              QMessageBox.Ok)
 
     @pyqtSlot()
     def on_click_set_path(self):
-        self.set_filename()
+        QFileDialog.getSaveFileName()
+        self.ui.lineEdit_save_image.setText(self.set_filename_open())
 
 
 if __name__ == "__main__":
--- rectangle_area.py
+++ rectangle_area.py
@@ -18,22 +18,23 @@ def create_area(*coords):
     return context
 
 
-def write_png(context):
+def write_png(context, filepath):
     image = context.render_cairo(800, 600)
-    image.write_to_png("rectangle_area_map.png")
+    image.write_to_png(filepath)
 
 
-def write_svg(context):
+def write_svg(context, filepath):
     svg_image = context.render_svg(800, 600)
-    with open("rectangle_area_map.svg", "w", encoding="utf-8") as f:
+    with open(filepath, "w", encoding="utf-8") as f:
         svg_image.write(f, pretty=True)
 
 
-def get_image(*coords, png_or_svg='png'):
+def get_image(*coords, filepath=None, png_or_svg='png'):
+    if filepath is None:
+        print('File is not saved.')
+        return
     context = create_area(*coords)
     if png_or_svg == 'png':
-        write_png(context)
+        write_png(context, filepath)
     elif png_or_svg == 'svg':
-        write_svg(context)
-
-
+        write_svg(context, filepath)
